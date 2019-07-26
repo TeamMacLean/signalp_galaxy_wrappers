dce9760a5675ab23c772740feb5f99db7223d533
Martin Page <wookoouk@gmail.com>
2019-07-26 15:35:53 +0100

    Update run_signalp5.sh

--- run_signalp5.sh
+++ run_signalp5.sh
@@ -4,4 +4,5 @@ organism=$1
 fasta_file=$2
 output_file=$3
 
-/tsl/software/testing/signalp/5.0b/x86_64/bin/signalp -batch 1000 -fasta ${fasta_file} -org ${organism} -stdout > ${output_file}
+cd /tsl/software/testing/signalp/5.0b/x86_64/bin
+./signalp -batch 1000 -fasta ${fasta_file} -org ${organism} -stdout > ${output_file}
